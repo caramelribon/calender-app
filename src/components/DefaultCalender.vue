1c0062323d767126436da548ae994d303d2217ef
caramelribon <caramelribon137@gmail.com>
2023-03-26 02:03:27 +0900

    [WIP] HomeViewの一部のUIを実装

---
+++ src/components/DefaultCalender.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="main__contents"></div>
+</template>
+
+<script lang="ts" setup>
+import { defineComponent } from 'vue'
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,38 @@
 <template>
   <div class="main__contents">
-    
+    <div class="about">
+      <p class="message">What's App Name?</p>
+      <p class="fw1 fs24 mt18 ml30">
+        App Nameはカレンダーをシェアしたり、<br />
+        みんなでカレンダーを作る上げたりするアプリです！
+      </p>
+    </div>
+    <div class="mt50">
+      <p class="en">Calender</p>
+      <p class="jp fw1 mt04">カレンダーの一覧</p>
+      <DefaultButton class="fw1 calender-btn" color="#BADF73"> カレンダーを作成する</DefaultButton>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { defineComponent } from 'vue'
-
-export default defineComponent({})
+import DefaultButton from '@/components/button/DefaultButton.vue'
 </script>
 
 <style scoped>
-h1 {
-  font-style: normal;
-  font-size: 72px;
-  line-height: 150%;
-  /* identical to box height, or 108px */
-
-  color: #fff57f;
+.message {
+  font-size: 64px;
+  line-height: 100%;
+  color: var(--white);
+  font-weight: 900;
   -webkit-text-stroke: 1px #000000;
+}
+.en {
+  color: var(--orange);
+}
+.calender-btn {
+  width: 314px;
+  margin: 50px auto 0 auto;
 }
 </style>
