c6cbaf5c07b2666bfa6178289a76bb4fe97b07a9
Nameless <tanisyun0331@icloud.com>
2023-03-25 21:11:42 +0900

    [add]スケジュール関係のダイアログの調整

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="main__contents">
+    <CreateCalenderDialogVue :is-open="true" @close="()=>{}"></CreateCalenderDialogVue>
   </div>
 </template>
 
 <script lang="ts">
+import CreateCalenderDialogVue from '@/components/CreateScheduleDialog.vue';
 import { defineComponent } from 'vue'
 
 export default defineComponent({
+    components: { CreateCalenderDialogVue }
 })
 </script>
 
